566ae7b34cd8202e48ad7876b69dd10a2a17fadd
cyuu0020 <cyuu0020@student.monash.edu>
2019-10-10 19:49:12 +1100

    inital

---
+++ FIT5032_Week08A/FIT5032_Week08A/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using FIT5032_Week08A.Models;
+using FIT5032_Week08A.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_Week08A.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Please comment out these codes once you have registered your API key.
+            //EmailSender es = new EmailSender();
+            //es.RegisterAPIKey();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Send_Email()
+        {
+            return View(new SendEmailViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Send_Email(SendEmailViewModel model, HttpPostedFileBase postedFile)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    String toEmail = model.ToEmail;
+                    String subject = model.Subject;
+                    String contents = model.Contents; 
+
+                    string serverPath = Server.MapPath("~/Uploads/");
+                    if (!Directory.Exists(serverPath))
+                    {
+                        Directory.CreateDirectory(serverPath);
+                    }
+                    string fileName = Path.GetFileName(postedFile.FileName);
+                    string path = serverPath + fileName;
+                    postedFile.SaveAs(serverPath + fileName);
+
+                    EmailSender es = new EmailSender();
+                    es.Send(toEmail, subject, contents, path, postedFile);
+
+                    ViewBag.Result = "Email has been send.";
+
+                    ModelState.Clear();
+
+                    return View(new SendEmailViewModel());
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase postedFile)
+        {
+            if (postedFile != null)
+            {
+                string path = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+
+                }
+                postedFile.SaveAs(path + Path.GetFileName(postedFile.FileName));
+                ViewBag.Message = "File uploaded successfully.";
+            }
+            return View();
+        }
+    }
+}
